fb087ab1876a7354686f1103e7c9db7dd1086158
billythekid <billythekid@github.com>
2025-02-24 07:05:30 +0000

    force vite?

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     @else
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+
     @endif
 </head>
 <body class="font-sans">
